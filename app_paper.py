f73f50766b84d1682156d33e514221baac81047d
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2022-02-21 08:22:32 +0100

    Update app_paper.py

--- app_paper.py
+++ app_paper.py
@@ -47,7 +47,7 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
         else:
             table = pd.read_csv(uploaded_file)
     else:
-        table = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/Complete_Vis_an_tool_paper/main/eter_mac_fin_wf.csv')
+        table = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/Complete_Vis_an_tool_paper/main/eter_fin_mac_wm.csv')
 
     # importing all other necessary files
     with urlopen('https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson') as response:
